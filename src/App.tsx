5c08c90ea1ad48b049e53265ed58c6661410dbcf
Subhasis Chakraborti <subchakraborti1@gmail.com>
2024-10-19 15:35:35 -0400

    ATTRIBUTE_LIST controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
 import { useState } from 'react';
 import './App.css';
-import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
-
+import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts';
+import { Attributes } from './types';
 
 function App() {
   const [num, setNum] = useState<number>(0);
+  const [attributeControls, setAttributeControls] = useState<Attributes>({
+    'Strength': 0,
+    'Dexterity': 0,
+    'Constitution': 0,
+    'Intelligence': 0,
+    'Wisdom': 0,
+    'Charisma': 0,
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Coding Exercise</h1>
       </header>
+      <section className="App-section">
+        {ATTRIBUTE_LIST.map((attribute, idx) => 
+          <div key={idx}>
+            <button onClick={() => setAttributeControls(attributeControls => ({ ...attributeControls, [attribute]: attributeControls[attribute] + 1 }))}>
+              Increase
+            </button>
+            <button onClick={() => setAttributeControls(attributeControls => ({ ...attributeControls, [attribute]: attributeControls[attribute] - 1 }))}>
+              Decrease
+            </button>
+            <span>{attribute}: {attributeControls[attribute]}</span>
+          </div>
+        )}
+      </section>
       <section className="App-section">
         <div>
           Value:
